a782b95ce5b378bf859d41206305df8dc5f504a9
Lesys <lesys.exia@gmail.com>
2018-09-06 20:20:29 +0200

    Ajout de l'idée du projet et des premières lignes

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NB_HEROS_COMMUN 40
+#define NB_HEROS_EPIQUE 20
+
+// Le but de ce programme est de faire des tableaux avec des numéros, mélangés, et des listes différentes
+int main() {
+
+	int heros_commun[NB_HEROS_COMMUN];
+        int heros_force[NB_HEROS_EPIQUE];
+        int heros_deter[NB_HEROS_EPIQUE];
+        int heros_chari[NB_HEROS_EPIQUE];
+
+	return 0;
+}
